3d0596d48784db5abb136ad22c178958f63cafcd
Will Nielsen <48866+wjn@users.noreply.github.com>
2021-02-20 17:18:05 -0600

    fixed typo in url-deleted-event

--- src/interfaces/events/url-deleted-event.ts
+++ src/interfaces/events/url-deleted-event.ts
@@ -3,6 +3,6 @@ import { UrlData } from '../data';
 import { BaseEvent } from './base-event';
 
 export interface UrlDeletedEvent extends BaseEvent {
-    topic: Topics.urlDeleted;
+    topic: Topics.UrlDeleted;
     data: UrlData;
 }
